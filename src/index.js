7cd2df890d2a9e1f20d90dbff87b61fc43eef8ea
Adam L <luzsiadam@gmail.com>
2021-12-03 21:49:53 +0100

    + Testing prop on main App component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App nevem="senki" />
   </React.StrictMode>,
   document.getElementById('root')
 );
