f2d12aa6f17ad5b55c0f3e95a3c85f4cfa157ef8
Adam L <luzsiadam@gmail.com>
2021-12-03 21:50:31 +0100

    + Testing state changes multiple ways

--- src/App.js
+++ src/App.js
@@ -30,14 +30,17 @@ import { SearchBox as SearchBuxa } from './components/search-box/search-box.comp
 
 class App extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       string: 'HUJJ HUJJ',
       josika: 'pistika',
       monsters: [{id: 'njmn', name: 'uff'}],
-      searchField: ''
+      searchField: '',
+      test2: true,
+      test3: false,
+      test4: this.props.nevem + 'ujj'
     }
     
     this.akarmi = new Promise(function(resolve, reject) {
@@ -152,6 +155,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" width="100"/>
           <p>{'{' + this.state.string + '}'}</p>
           <button onClick={() => this.setState({ string: 'Jóska ' + (new Date()).getSeconds() })}>Klikk</button>
+          <p style={{color: 'white'}}>{this.state.test2 ? 'true' : 'false'}</p>
+          <button onClick={() => this.setState(state => ({ test2: !state.test2 }))}>Klikk 2</button>
+          <button onClick={() => this.setState(state => { return {test2: !state.test2 }})}>Klikk 2v2</button>
+          <p style={{color: 'white'}}>{this.state.test3 ? 'true' : 'false'}</p>
+          <button onClick={() => this.setState((prevState, prevProps) => { console.log(prevState); console.log(prevProps); return {test3: !prevState.test3 }})}>Klikk 3</button>
           <br/>
           <br/>
           <br/>
